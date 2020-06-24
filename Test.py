927cc29389cdadbad8eee2cc890bd96db470b67b
BRutan <rutan.benjamin@gmail.com>
2020-06-23 20:21:19 -0400

    Initial commit.

---
+++ Test.py
@@ -0,0 +1,13 @@
+######################################
+# Test.py (DeepFactorAlpha)
+######################################
+# Description:
+# * Testing the implementation of the 
+# Deep learning factor alpha (Feng et al 2018).
+
+
+def Test():
+    pass
+
+if __name__ == "__main__":
+    Test()
